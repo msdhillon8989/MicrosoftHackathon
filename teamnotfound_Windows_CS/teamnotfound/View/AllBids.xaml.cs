ccb10237547a94136892d445e2a80acd417f5c86
Maninder Singh <maninders@hpe.com>
2016-06-01 11:12:16 +0530

    Updated

--- teamnotfound_Windows_CS/teamnotfound/View/AllBids.xaml.cs
+++ teamnotfound_Windows_CS/teamnotfound/View/AllBids.xaml.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using Microsoft.WindowsAzure.MobileServices;
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using teamnotfound.DataModel;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -25,7 +29,88 @@ namespace teamnotfound.View
         public AllBids()
         {
             this.InitializeComponent();
-            int x = 0;
+          
+
+        }
+
+        string[] bid;
+        List<CountryBasedBidCount> countryBidCount = new List<CountryBasedBidCount>();
+       
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            string event1 = e.Parameter as String;
+            bid = event1.Split('|');
+            Debug.Write("param1: " + bid[0]+"   "+bid[1]+" "+bid[2]);
+            //getProjects(parameter);
+            //createCountry();
+            getCountry();
+
+        }
+        List<ShowUserBid> allbids = new List<ShowUserBid>();
+        IMobileServiceTable<Bid> bidTable = App.MobileService.GetTable<Bid>();
+        private async void getCountry()
+        {
+
+            allbids.Clear();
+            MobileServiceCollection<Bid, Bid> bids = await bidTable.Where(Bid => Bid.EventId == bid[0].ToString()  && Bid.Countr == bid[1].ToString() ).ToCollectionAsync();
+
+
+            foreach (var _bid in bids)
+            {
+
+                ShowUserBid b = new ShowUserBid();
+
+                b.username = _bid.Bidder;
+                b.bid = _bid.BiddAmt;
+                b.eventid = _bid.EventId;
+                b.bidId = _bid.Id;
+                if(bid[2]=="Open")
+                {
+                    b.showButton = "Visible";
+                    b.showAssigned = "Collapsed";
+                }
+                else
+                {
+                    b.showButton = "Collapsed";
+                    b.showAssigned = "Collapsed";
+                    if (_bid.Status!=null && _bid.Status=="Accepted")
+                    {
+                        b.showAssigned = "Visible";
+                    }
+                }
+                allbids.Add(b);
+                Debug.WriteLine("Bidder  "+b.username+ "  Bid"+b.bid);
+            }
+
+
+
+
+            allBidsList.ItemsSource = allbids;
+
+
+        }
+
+        private async void StackPanel_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            var selected = allBidsList.SelectedIndex;
+            Debug.WriteLine(selected + "  ");
+            ShowUserBid selectedBid = allbids.ElementAt(selected);
+
+
+            for (int i=0; i < allbids.Count; i++)
+            {
+                ShowUserBid _bid = allbids.ElementAt(i);
+                string status = "Rejected";
+                if(_bid.username == selectedBid.username)
+                {
+                    status = "Accepted";
+                }
+
+                Bid bidding = new Bid { Id = _bid.bidId, BiddAmt = _bid.bid, Bidder = _bid.username, EventId = bid[0], Countr = bid[1], Status = status};
+                await bidTable.UpdateAsync(bidding);
+                getCountry();
+            }
 
         }
     }
--- teamnotfound_Windows_CS/teamnotfound/View/AllBidsCountry.xaml.cs
+++ teamnotfound_Windows_CS/teamnotfound/View/AllBidsCountry.xaml.cs
@@ -16,6 +16,7 @@ namespace teamnotfound.View
     public sealed partial class AllBidsCountry : Page
     {
         string event1;
+        List<CountryBasedBidCount> countryBidCount = new List<CountryBasedBidCount>();
         public AllBidsCountry()
         {
             this.InitializeComponent();
@@ -30,19 +31,19 @@ namespace teamnotfound.View
             getCountry();
 
         }
-
+        Hashtable stht = new Hashtable();
         private async void getCountry()
         {
             IMobileServiceTable<Bid> bidTable = App.MobileService.GetTable<Bid>();
             Hashtable ht = new Hashtable();
-            Hashtable stht = new Hashtable();
+           
             MobileServiceCollection<Bid, Bid> bids = await bidTable.Where(Bid => Bid.EventId == event1.ToString()).ToCollectionAsync();
             
             foreach (var _bid in bids)
             {
                 var country = _bid.Countr;
                 var count = ht[country];
-                if (stht[country] == null)
+                if (!stht.ContainsKey(country))
                 {
                     stht[country] = "Open";
                 }
@@ -52,18 +53,19 @@ namespace teamnotfound.View
                     stht[country] = "Assigned";
                 }
 
-                if(count==null)
+                if(!ht.ContainsKey(country))
                 {
                     ht.Add(country, 1);
                 }
                 else
                 {
+                    
                     var x = Int32.Parse(count.ToString()) + 1;
-                    ht.Add(country, x);
+                    ht[country] = x;
                 }
             }
 
-            List<CountryBasedBidCount> countryBidCount = new List<CountryBasedBidCount>();
+          
 
             ICollection key = ht.Keys;
 
@@ -83,5 +85,13 @@ namespace teamnotfound.View
 
 
         }
-}
+
+        private void StackPanel_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            var selected = countryBidCountlist.SelectedIndex;
+            CountryBasedBidCount eventcountry = countryBidCount.ElementAt(selected);
+
+            Frame.Navigate(typeof(AllBids), event1+"|"+eventcountry.country+"|"+stht[eventcountry.country].ToString());
+        }
+    }
 }
